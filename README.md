7f87a40b40751076ec5f27084e440fb98814ed77
Stéphane <steph@Asus.home>
2020-06-01 10:21:45 +0200

    15 - Symfony : EntityType

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
+09 - Symfony : Les relations bidirectionnelles avec Doctrine :
 https://www.loom.com/share/8844bf42b9d24923a411ecd9bb4aaf2c
 
+
+10 - Symfony : Le param converter :
 https://www.loom.com/share/7dd03d90c7304f6e82ae472aa955efe1
 
+
+11 - Symfony : Gardez la “form” ! :
 https://www.loom.com/share/1a3633abb0b5454b9b000f9f98f0d361
 
+
+12 - Symfony : Générer un CRUD :
 https://www.loom.com/share/515eba123a7f43e6a30d9799d5c10278
 
+
+13 - Symfony : Validation :
 https://www.loom.com/share/5d4d0e458bbe42a798f819c2fc4c7f5b
 
+
+14 - Symfony : Doctrine relations “Many-To-Many” :
 https://www.loom.com/share/2729219cfaa3406fb4063e3304a1a5aa
+
+15 - Symfony : EntityType :
+https://www.loom.com/share/f35f9303901946318b49948de766f5d7
--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,13 +14,20 @@ class ProgramType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title',null, ['label'=>'Titre', 'attr' => ['maxlength'=>300]])
+            ->add('title', null, ['label' => 'Titre', 'attr' => ['maxlength' => 300]])
             ->add('synopsis')
-            ->add('poster',null, ['label'=>'Image'])
-            ->add('country',null, ['label'=>'Pays'])
-            ->add('year',null, ['label'=>'Année'])
+            ->add('poster', null, ['label' => 'Image'])
+            ->add('country', null, ['label' => 'Pays'])
+            ->add('year', null, ['label' => 'Année'])
             ->add('category', null, ['choice_label' => 'name', 'label' => 'Catégorie'])
-        ;
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'expanded' => true,
+                'multiple' => true,
+                'by_reference' => false,
+                'label' => 'Acteur/s',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
